6c02bffd7a3371c89bf1c627c81eba82f35526df
viniciusferreira001 <vinicius.assis@ufv.br>
2022-10-05 01:31:25 -0300

    done

---
+++ folhaPagamento/exercicio.java
@@ -0,0 +1,107 @@
+package folhaPagamento;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class exercicio {
+    public static void main(String[] args) {
+        int opcao = 0;
+        funcionario empregado = new funcionario();
+        ArrayList<funcionario> funcionarios = new ArrayList<>(); // Cria um array de funcionários
+        Scanner entrada = new Scanner(System.in);
+
+        do{
+            
+            System.out.println("Digite a opção desejada: ");
+            System.out.println("1 - Inicializar o nome e a data de nascimento do funcionário");
+            System.out.println("2 - Imprimir os dados do funcionário");
+            System.out.println("3 - Calcular o sálario bruto do funcionário");
+            System.out.println("4 - Calcular o sálario líquido do funcionário");
+            System.out.println("5 - Calcular o valor do desconto do INSS");
+            System.out.println("6 - Calcular o valor do desconto do IRRF");
+            System.out.println("7 - Mostrar folha de pagamento do funcionário");
+            System.out.println("8 - Alterar dados dos funcionários");
+            System.out.println("-1 - Sair");
+            opcao = entrada.nextInt();
+            switch (opcao) {
+                case 1:
+                    empregado = new funcionario();
+                    empregado.iniciarAtributos(empregado);
+                    funcionarios.add(empregado);
+                    break;
+                
+                case 2:
+                    if(funcionarios.size() > 0){
+                        for (int i = 0; i < funcionarios.size(); i++) {
+                            funcionarios.get(i).getDados();
+                        }
+                    }else{
+                        System.out.println("Não há funcionários cadastrados!");
+                    }
+                    break;
+                
+                case 3:
+                    System.out.println("Digite o numero do funcionario a calcular o salario");
+                    int i = entrada.nextInt();
+                    System.out.println("Digite a quantidade de horas trabalhadas do funcionario que deseja calcular o salário bruto: ");
+                    double horasDeTrabalho = entrada.nextDouble();
+                    double saida = funcionarios.get(i).salarioBruto(horasDeTrabalho);
+                    System.out.printf("O salário bruto eh: %f\n", saida);
+                    break;
+                
+                case 4:
+                    System.out.println("Digite o numero do funcionario a calcular o salario líquido");
+                    int num = entrada.nextInt();
+                    System.out.println("Digite a quantidade de horas trabalhadas do funcionario que deseja calcular o salário bruto: ");
+                    double horas = entrada.nextDouble();
+                    System.out.println("Salário líquido: " + funcionarios.get(num).salarioLiquido(horas));
+                    break;
+
+                case 5:
+                    System.out.println("Digite o numero do funcionario a calcular o desconto INSS:");
+                    int numFuncionario = entrada.nextInt();
+                    System.out.println("Desconto INSS eh: " + funcionarios.get(numFuncionario).descontoINSS());
+                    break;
+                
+                case 6:
+                    System.out.println("Digite o numero do funcionario a calcular o desconto IRRF:");
+                    int numFuncionario2 = entrada.nextInt();
+                    System.out.println("Desconto IRRF eh: " + funcionarios.get(numFuncionario2).descontoIR());
+                    break;
+                
+                case 7:
+                    System.out.println("Digite o numero do funcionario a calcular o salario líquido");
+                    int num1 = entrada.nextInt();
+                    System.out.println("Digite a quantidade de horas trabalhadas do funcionario que deseja calcular o salário bruto: ");
+                    double horas1 = entrada.nextDouble();
+                    
+                    System.out.println("FOLHA DE PAGAMENTO:");
+                    System.out.println("Salário bruto: " + funcionarios.get(num1).salarioBruto(horas1));
+                    System.out.println("Desconto INSS: " + funcionarios.get(num1).descontoINSS());
+                    System.out.println("Desconto IRRF: " + funcionarios.get(num1).descontoIR());
+                    System.out.println("Salário líquido: " + funcionarios.get(num1).salarioLiquido(horas1));
+                    break;
+                    
+                case 8:
+                    
+                    System.out.println("Digite o numero do funcionario a alterar os dados:");
+                    int j = entrada.nextInt();
+                    System.out.println("Digite o novo tipo de contrato do funcionario:");
+                    funcionarios.get(j).setTipoContrato(entrada.next().charAt(0));
+                    System.out.println("Digite o novo numero de dependentes do funcionario:");
+                    funcionarios.get(j).setNumFilhosMenores(entrada.nextInt());
+                    System.out.println("Digite o novo salário do funcionário:");
+                    funcionarios.get(j).setSalario(entrada.nextDouble());
+                    System.out.println("Digite a nova data de admissão do funcionário:"); 
+                    funcionarios.get(j).setDataAdmissao(entrada.next());
+                    break;
+
+        
+                default:
+                    System.out.println("Opção inválida!");
+            } 
+            
+        }while (opcao != -1);
+
+        entrada.close();
+    }
+}
---
+++ folhaPagamento/funcionario.java
@@ -0,0 +1,137 @@
+package folhaPagamento;
+import java.util.Scanner;
+
+public class funcionario {
+    String nome;
+    String dataNascimento;
+    char tipoContrato; // E - Efetivo, H - Horista
+    double salario;
+    int numFilhosMenores;
+    String dataAdmissao;
+
+    Scanner entrada = new Scanner(System.in);
+    
+    public void iniciarAtributos(funcionario funcionarioEmpregado){
+        iniciaNome(funcionarioEmpregado);
+        iniciaDataNascimento(funcionarioEmpregado);
+        contratar(funcionarioEmpregado);
+    }
+    public void iniciaNome(funcionario funcionarioEmpregado){
+        System.out.println("Digite o nome do funcionário: ");
+        this.nome = entrada.nextLine();
+    }
+    public void iniciaDataNascimento(funcionario funcionarioEmpregado){
+        System.out.println("Digite a data de nascimento do funcionário: ");
+        this.dataNascimento = entrada.nextLine();
+    }
+    
+    public void contratar(funcionario funcionarioEmpregado){
+        iniciaTipoContrato(funcionarioEmpregado);
+        iniciaSalario(funcionarioEmpregado);
+        iniciaNumFilhosMenores(funcionarioEmpregado);
+        iniciaDataAdmissao(funcionarioEmpregado);
+    }
+    public void iniciaTipoContrato(funcionario funcionarioEmpregado){
+        System.out.println("Digite o tipo de contrato do funcionário: ");
+        this.tipoContrato = entrada.next().charAt(0);        
+    }
+    public void iniciaSalario(funcionario funcionarioEmpregado){
+        System.out.println("Digite o salário do funcionário: ");
+        this.salario = entrada.nextDouble();
+    }
+    public void iniciaNumFilhosMenores(funcionario funcionarioEmpregado){
+        System.out.println("Digite o número de filhos menores do funcionário: ");
+        this.numFilhosMenores = entrada.nextInt();
+    }
+    public void iniciaDataAdmissao(funcionario funcionarioEmpregado){
+        System.out.println("Digite a data de admissão do funcionário: ");
+        this.dataAdmissao = entrada.next();
+    }
+    public void getDados(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Data de Nascimento: " + dataNascimento);
+        System.out.println("Tipo de Contrato: " + tipoContrato);
+        System.out.println("Salário: " + salario);
+        System.out.println("Número de Filhos Menores: " + numFilhosMenores);
+        System.out.println("Data de Admissão: " + dataAdmissao);
+    }
+    public String getNome(){
+        return nome;
+    }
+    public char getTipoContrato(){
+        return tipoContrato;
+    }
+    public double getSalario(){
+        return salario;
+    }
+    public int getNumFilhosMenores(){
+        return numFilhosMenores;
+    }
+    public String getDataAdmissao(){
+        return dataAdmissao;
+    }
+
+
+    public void setTipoContrato(char tipoContrato){
+        this.tipoContrato = tipoContrato;
+    }
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+    public void setNumFilhosMenores(int numFilhosMenores){
+        this.numFilhosMenores = numFilhosMenores;
+    }
+    public void setDataAdmissao(String dataAdmissao){
+        this.dataAdmissao = dataAdmissao;
+    }
+    
+    
+    public double salarioBruto(double horasTrabalho){
+        if(Character.compare(tipoContrato, 'H') == 0){
+            double salarioBruto = salario * horasTrabalho;
+            return salarioBruto;
+        }
+        else{
+            return salario;
+        }
+    }
+    public double descontoINSS(){
+        if(salario<=1659.38){
+            return salario*0.08;
+        }
+        else if(salario>1659.38 && salario<=2765.66){
+            return salario*0.09;
+        }
+        else if(salario>2765.66 && salario<=5531.31){
+            return salario*0.11;
+        }
+        else{
+            return 5531.31*0.11;
+        }
+    }
+    public double descontoIR(){
+        if(salario<=1903.98){
+            return 0;
+        }
+        else if(salario>1903.98 && salario<=2826.65){
+            return salario*0.075 - 142.80;
+        }
+        else if(salario>2826.65 && salario<=3751.05){
+            return salario*0.15 - 354.80;
+        }
+        else if(salario>3751.05 && salario<=4664.68){
+            return salario*0.225 - 636.13;
+        }
+        else{
+            return salario*0.275 - 869.36;
+        }
+    }
+    public double salarioLiquido(double horasTrabalho){
+        return salarioBruto(horasTrabalho) - descontoINSS() - descontoIR();
+    }
+
+    
+
+    
+   
+}
